e74b96e69b495817e443f1bb8ad0635aca93fb70
Max-Qi <max.qi@mail.utoronto.ca>
2019-10-27 01:03:51 -0400

    Finished first part

--- main.py
+++ main.py
@@ -2,11 +2,23 @@ import praw
 from praw.models import MoreComments
 from textblob import TextBlob
 
+def analyze_comment(comment):
+    analysis = TextBlob(comment.body)
+    weight = comment.score * analysis.polarity
+    return weight
+
 def depth_first_comment_iteration (level, comments):
     comments.replace_more(limit=None)
+    weight = 0
     for comment in comments:
-        print (level, 3 * level * ' ', comment.body)
-        depth_first_comment_iteration (level + 1, comment.replies)
+        new_weight = analyze_comment(comment)
+        weight += new_weight
+        print (new_weight, 3 * level * ' ', comment.body)
+        weight += depth_first_comment_iteration (level + 1, comment.replies)
+    return weight
+
+def extract_name(title):
+
     return
 
 def main():
@@ -17,12 +29,19 @@ def main():
 
     subreddit_name = 'hiphopheads'
     subreddit = reddit.subreddit(subreddit_name)
-    hots = reddit.subreddit(subreddit_name).hot(limit = 2)
+    hots = reddit.subreddit(subreddit_name).hot(limit = 1)
     tops = reddit.subreddit(subreddit_name).top('day', limit = 1)
+
+    weight = 0
 
     for post in tops:
         print(post.title, '\n')
-        depth_first_comment_iteration(0, post.comments)
+        extract_name(post.title)
+        testBlob = TextBlob(post.title)
+        print(testBlob.pos_tags)
+        weight += depth_first_comment_iteration(0, post.comments)
+
+    print ('The total weight is ' , weight)
 
 if __name__ == "__main__":
     main()
